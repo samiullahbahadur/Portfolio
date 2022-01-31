50bd98a6d1acf608e083f100b67de7e83cac527a
samiullahbahadur <bahadursamiullah@gmail.com>
2022-01-31 17:50:09 +0430

    Install script linters

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-const navMenu = () => {
-  const burger = document.querySelector(".humberger-menu");
-  const nav = document.querySelector(".desktop-nav");
-
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    burger.classList.toggle("toggle");
+const navSlide = () => {
+  const burger = document.querySelector('.humberger-menu');
+  const nav = document.querySelector('.desktop-nav');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
   });
-  document.querySelectorAll(".nav-link").forEach((n) =>
-    n.addEventListener("click", () => {
-      nav.classList.remove("nav-active");
-      burger.classList.remove("toggle");
-    })
-  );
+  document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.remove('toggle');
+  }));
 };
-navMenu();
-// single Project section modal
+
+navSlide();
